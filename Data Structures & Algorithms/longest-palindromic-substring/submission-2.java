a65eedc111b489607413b69f9774ace10991e27f
Alessandro Grazioli <86263895+AlexGrazioli@users.noreply.github.com>
2026-05-01 10:45:22 +0200

    Add: longest-palindromic-substring - submission-2

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-2.java
@@ -0,0 +1,27 @@
+class Solution {
+    int maxLength = 0;
+    int start = 0;
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() == 1) return s;
+
+        for (int i = 0; i < s.length(); i++) {
+            expandAroundTheCenter(s, i, i);
+            expandAroundTheCenter(s, i, i + 1);
+        }
+
+        return s.substring(start, start + maxLength);
+    }
+
+    private void expandAroundTheCenter(String s, int left, int right) {
+        while (left >= 0 & right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        int currentLength = right - left - 1;
+        if (currentLength > maxLength) {
+            maxLength = currentLength;
+            start = left + 1;
+        }
+    }
+}
